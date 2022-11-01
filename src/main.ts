e39ab71570b5e3cee704006d172d171fdfb656ae
Silas P Ladislau <splhead@gmail.com>
2022-11-01 17:30:22 -0400

    test create

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { Sqlite3Datasource } from './datasources/connectors/sqlite3datasource'
 import path from 'path'
 import { XMLParser } from 'fast-xml-parser'
 import { readFilePromise } from './file'
-import { simplifyObject } from './utils/functions'
+import { isLeaf, Leaf, simplifyObject, Tree } from './utils/functions'
 
 const main = async () => {
   const datasource = new Sqlite3Datasource('msc.db')
-  const sql = 'SELECT * FROM msc LIMIT 1'
+  const sql = 'SELECT * FROM msc LIMIT 2'
 
   const data = await datasource.getData(sql)
   console.log(data)
@@ -21,61 +21,107 @@ const main = async () => {
   })
 
   const xmlObject = parser.parse(templateFile)
-  const xbrlSimplified = simplifyObject(xmlObject)
+  const xbrlSimplified = simplifyObject(xmlObject, 'gl-cor:entryDetail')
+
+  // TODO: ask which element is repeatable
 
   // eslint-disable-next-line
-  console.log(
-    JSON.stringify(
-      xbrlSimplified['xbrli:xbrl']['gl-cor:accountingEntries'][
-        'gl-cor:entryHeader'
-      ]['gl-cor:entryDetail'][0],
-    ),
-  )
+  // console.log(
+  //   JSON.stringify(
+  //     xbrlSimplified['xbrli:xbrl']['gl-cor:accountingEntries'][
+  //       'gl-cor:entryHeader'
+  //     ]['gl-cor:entryDetail'][0],
+  //   ),
+  // )
+
+  // TODO: generate each element for each data row
+
+  function addChild(parent: Tree, name: string): Record<string, unknown> {
+    return Object.assign(parent, { [name]: {} })
+  }
+
+  function addAttribute(element: Tree, name: string, value: string | number) {
+    return Object.assign(element, { [name]: value })
+  }
+
+  const newElement = {}
+  addChild(newElement, 'gl-cor:lineNumberCounter')
+  addAttribute(newElement['gl-cor:lineNumberCounter'], '#text', 1)
+  console.log(newElement)
+  // data.forEach((row) => {
+  //   console.log(
+  //     teste(
+  //       row as Source,
+  //       [
+  //         {
+  //           'gl-cor:lineNumberCounter': {
+  //             '#text': 1,
+  //             '@_contextRef': 'C1',
+  //             '@_decimals': '0',
+  //             '@_unitRef': 'u',
+  //           },
+  //           'gl-cor:account': {
+  //             'gl-cor:accountMainID': {
+  //               '#text': 111110100,
+  //               '@_contextRef': 'C1',
+  //             },
+  //             'gl-cor:accountSub': [
+  //               {
+  //                 'gl-cor:accountSubID': {
+  //                   '#text': 10131,
+  //                   '@_contextRef': 'C1',
+  //                 },
+  //                 'gl-cor:accountSubType': {
+  //                   '#text': 'PO',
+  //                   '@_contextRef': 'C1',
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           'gl-cor:amount': {
+  //             '#text': 1388187.15,
+  //             '@_contextRef': 'C1',
+  //             '@_decimals': '2',
+  //             '@_unitRef': 'BRL',
+  //           },
+  //           'gl-cor:debitCreditCode': { '#text': 'C', '@_contextRef': 'C1' },
+  //           'gl-cor:xbrlInfo': {
+  //             'gl-cor:xbrlInclude': {
+  //               '#text': 'beginning_balance',
+  //               '@_contextRef': 'C1',
+  //             },
+  //           },
+  //         },
+  //       ],
+  //       ['conta', 'tipo_valor'],
+  //     ),
+  //   )
+  // })
 }
 
 main()
-type Source = Record<string, string | number>
-function teste(
-  source: Source,
-  xbrl: Record<string, string | number>,
-  inputs: string[],
-) {
-  const state = new Map()
-  for (const [key, value] of Object.entries(xbrl)) {
-    const input = inputs.shift()
-    if (!input) {
-      throw Error('Wrong input' + input)
-    }
-    if (typeof input !== typeof value) {
-      throw Error(`Typeof input ${input} is diferente from ${value} in ${key}`)
-    }
-    const sourceValue = source[input]
-    state.set(key, sourceValue)
-  }
-  return Object.fromEntries(state.entries())
-}
+// type Source = Record<string, string | number>
 
-console.log(
-  teste(
-    {
-      conta: '111110100',
-      ic1: '10131',
-      tipo1: 'PO',
-      ic2: '1',
-      tipo2: 'FP',
-      ic3: '1500',
-      tipo3: 'FR',
-      ic4: '',
-      tipo4: '',
-      ic5: '',
-      tipo5: '',
-      ic6: '',
-      tipo6: '',
-      valor: 1388187.15,
-      tipo_valor: 'beginning_balance',
-      natureza_valor: 'C',
-    },
-    { conta: 'b', valor: 'aaa' },
-    ['conta', 'tipo_valor'],
-  ),
-)
+// function teste(source: Source, xbrl: Tree, inputs: string[]) {
+//   const state = new Map()
+//   for (const [key, value] of Object.entries(xbrl)) {
+//     if (isLeaf(value)) {
+//       if (key.startsWith('#text')) {
+//         const input = inputs.shift()
+//         if (!input) {
+//           throw Error('Wrong input' + input)
+//         }
+//         if (typeof input !== typeof value) {
+//           throw Error(
+//             `Typeof input ${input} is diferente from ${value} in ${key}`,
+//           )
+//         }
+//         const sourceValue = source[input]
+//         state.set(key, sourceValue)
+//       }
+//     } else {
+//       teste(source, value, inputs)
+//     }
+//   }
+//   return Object.fromEntries(state.entries())
+// }
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,26 +1,38 @@
 type Element = {
   [K: string]: Element | Array<Element> | string
 }
-// type ParsedElement = {
-//   [K: string]: Element | [ParsedElement] | string
-// }
 
-export function simplifyObject(element: Element): Element {
+export type Leaf = string | number
+
+export type Tree =
+  | Array<Tree>
+  | Readonly<{
+      [k in string]: Tree | Leaf
+    }>
+
+export function simplifyObject(
+  element: Element | Element[],
+  repeatableKey: string,
+): Element {
   const entries = Object.entries(element)
 
   return Object.fromEntries(
     entries.map(([key, value]) => {
-      if (Array.isArray(value)) {
+      if (typeof value === 'string' || typeof value === 'number') {
+        return [key, value]
+      }
+      if (key === repeatableKey && Array.isArray(value)) {
         const [head] = value
 
         if (!head) throw new Error(`Arquivo inválido! ${key}`)
 
-        return [key, [simplifyObject(head)]]
+        return [key, [simplifyObject(head, repeatableKey)]]
       }
-      if (typeof value === 'string' || typeof value === 'number') {
-        return [key, value]
-      }
-      return [key, simplifyObject(value)]
+      return [key, simplifyObject(value, repeatableKey)]
     }),
   )
+}
+
+export function isLeaf<T>(data: T | Leaf): data is Leaf {
+  return typeof data === 'string' || typeof data === 'number'
 }
